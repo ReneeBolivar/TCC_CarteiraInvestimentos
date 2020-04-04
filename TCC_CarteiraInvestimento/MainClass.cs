41643099e189ca0b8a9773b346ff6e8f51046d9c
Renee Bolivar <reneegois@hotmail.com.br>
2020-04-04 19:02:52 -0300

    Criado projeto .net Core 3.0

---
+++ TCC_CarteiraInvestimento/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCC_CarteiraInvestimento
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
