ea2adf4d9ee28399b11e7194c33785bde9e22743
Renee Bolivar <reneegois@hotmail.com.br>
2020-04-04 19:19:33 -0300

    Criado utilitarios

---
+++ TCC_CarteiraInvestimento/Utils/Utilitario.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TCC_CarteiraInvestimento.Utils
+{
+    public static class Utilitario
+    {
+        public static T Clonar<T>(this T @object)
+        {
+            var deserializeSettings = new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace };
+
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(@object), deserializeSettings);
+        }
+
+        public static int ObterNumeroRandom(int max, int min)
+        {
+            Thread.Sleep(100);
+            return new Random().Next(min, max);
+        }
+
+        public static bool Equals(this Individuo individuo, Individuo individuoComparar)
+        {
+            foreach (var cromossomo in individuo.Cromossomos)
+                if (!individuoComparar.Cromossomos.Contains(cromossomo))
+                    return false;
+
+            return true;
+        }
+    }
+}
